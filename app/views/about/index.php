ba8892ecdd6abcf4c31ac604d8f13bbe3c70c5a2
Raihantrias <raihan301004@gmail.com>
2024-11-21 19:18:16 +0700

    Initial commit

---
+++ app/views/about/index.php
@@ -0,0 +1,50 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Halaman AboutMe</h1>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card card-primary">
+        <div class="card-header">
+          <h3 class="card-title">Hello</h3>
+        </div>
+        <div class="card-body box-profile">
+
+          <h3 class="profile-username text-center">Raihan Tri Aji Santoso</h3>
+          <h3 class="profile-username text-center">2210010223</h3>
+
+          <p class="text-muted text-center">Web Developer</p>
+
+          <ul class="list-group list-group-unbordered mb-3">
+            <li class="list-group-item">
+              <strong><i class=""></i> Tempat Lahir</strong>
+              <a class="float-right"> Banjarmasin</a>
+            </li>
+            <li class="list-group-item">
+              <strong><i class=""></i> Tempat tinggal</strong>
+              <a class="float-right"> Jl.Antasan Raden</a>
+            </li>
+            <li class="list-group-item">
+              <strong><i class=""></i> Hobi</strong>
+              <a class="float-right"> Bermain Futsal,Bermain Bola,Badminton </a>
+          </ul>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
